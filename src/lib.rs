24247cd355a59bbc8eab1d9e1eff3a8639665341
manthanmabc <manthanmji@gamil.com>
2021-03-14 11:49:50 +0530

    added mouse interactivity

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ use sdl2::video::Window;
 pub use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::rect::Point;
+pub use sdl2::render::BlendMode;
+use sdl2::mouse::RelativeMouseState;
 
 pub struct Draw {
-	// sdl_context: sdl2::Sdl,
 	event_pump: sdl2::EventPump,
-	// video_subsystem: sdl2::VideoSubsystem,
-	// window:sdl2::video::Window,
-	canvas: sdl2::render::Canvas<Window>
+	canvas: sdl2::render::Canvas<Window>,
+	mosue_state: RelativeMouseState,
 }
 
 impl Draw {
@@ -17,13 +17,19 @@ impl Draw {
 	pub fn new(width: u32, height: u32) -> Draw{
 
 		let sdl_context = sdl2::init().unwrap();
+		
+		// Hide the cursor
+		sdl_context.mouse().show_cursor(false);
+
 		let event_pump = sdl_context.event_pump().unwrap();
 		let video_subsystem = sdl_context.video();
 		
-		// let window = WindowBuilder::new("Example", 800, 600).build().unwrap();
+		let mosue_state = RelativeMouseState::new(&event_pump);
+
+		// create a window 
 		let window = video_subsystem.unwrap().window("Example", width, height).build().unwrap();
 		
-		// Let's create a Canvas which we will use to draw in our Window
+		// create a Canvas will use to draw in our Window
 		let mut canvas : Canvas<Window> = window.into_canvas()
 		    .present_vsync() //< this means the screen cannot
 		    // render faster than your display rate (usually 60Hz or 144Hz)
@@ -31,13 +37,17 @@ impl Draw {
 
 		canvas.clear();
 
-		Draw {canvas, event_pump}
+		Draw {canvas, event_pump, mosue_state}
 
 	}
 
 	pub fn rect(&mut self, x: i32, y: i32, width: u32, height:u32) {
 		self.canvas.fill_rect(Rect::new(x, y, width, height)).unwrap();
-		self.canvas.present();
+		// self.canvas.present();
+	}
+
+	pub fn present(&mut self){
+		self.canvas.present();	
 	}
 
 	pub fn stroke(&mut self, col: Color) {
@@ -48,7 +58,7 @@ impl Draw {
 		let point1 = Point::new(x1, y1);
 		let point2 = Point::new(x2, y2);
 
-		self.canvas.draw_line(point1, point2);
+		self.canvas.draw_line(point1, point2).unwrap();
 		self.canvas.present();
 	}
 
@@ -60,22 +70,26 @@ impl Draw {
             }
 		}
 	}
+
+	pub fn clear(&mut self) {
+		self.canvas.set_draw_color(Color::RGB(0, 0, 0));
+		// fills the canvas with the color we set in `set_draw_color`.
+		self.canvas.clear();
+	}
+
+	pub fn blend_mode(&mut self, mode: BlendMode) {
+		self.canvas.set_blend_mode(mode);
+	}
+
+	pub fn is_mouse_pressed(&self) -> bool {
+		self.event_pump.mouse_state().left()
+	}
+
+	pub fn mouse_x(&self) -> i32 {
+		self.event_pump.mouse_state().x()
+	}
+
+	pub fn mouse_y(&self) -> i32 {
+		self.event_pump.mouse_state().y()
+	}
 }
-
-pub fn point(x:i32, y:i32) {
-
-}
-
-// canvas.set_draw_color(Color::RGB(0, 0, 0));
-// canvas.clear();
-
-// canvas.set_draw_color(Color::RGB(255, 210, 0));
-// canvas.fill_rect(Rect::new(10, 10, 780, 580)).unwrap();
-
-// canvas.set_draw_color(Color::RGB(0, 0, 0));
-
-// for x in 0..800 {
-// canvas.draw_point(Point::new(x, 300)).unwrap();		
-// }
-
-// canvas.present();
